c40c18815411d56dcd3b269b42f2afc1af932135
june <annie.chang@logncoding.com>
2024-04-21 15:31:31 +0900

    created my own fm

--- spotify/base_file.py
+++ spotify/base_file.py
@@ -18,29 +18,26 @@ class lastFmSpotify:
         params = {'limit': 20, 'api_key': self.api_key}
         url = f'https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json'
         response = requests.get(url, params = params)
-        if response.status_code != 200:
-            print("ERROR")
         res = response.json()
+
         for item in res['tracks']['track']:
-            song = item['name'].title()
+            song = item['name']
             artist = item['artist']['name']
             self.song_info[song] = artist
-        self.get_uri_from_spotify()
-
+        
     def get_uri_from_spotify(self):
-        for song_name, artist in self.song_info.items():
-            url = f"https://api.spotify.com/v1/search?query=track%3A{song_name}+artist%3A{artist}&type=track&offset=0&limit=10"
-            response = requests.get(url, headers = self.headers)
-            print(response.status_code)
+        for (song, artist) in self.song_info.items():
+            url=f"https://api.spotify.com/v1/search?query=track%3A{song}+artist%3A{artist}&type=track&offset=0&limit=10"
+            response = requests.get(url, headers=self.headers)
             res = response.json()
-            output_uri = res['tracks']['items']
-            #print(output_uri[0])
-            self.uris.append(output_uri[0]["uri"])
+            uri = res["tracks"]["items"][0]["uri"]
+            self.uris.append(uri)
+        print(self.uris)
 
     def create_spotify_playlist(self):
         data = {
-            "name": "LastFM top songs",
-            "description": "songs from the top tracks",
+            "name": "Last FM top 20",
+            "description": "New playlist description",
             "public": True
         }
         data = json.dumps(data)
@@ -50,21 +47,28 @@ class lastFmSpotify:
             print("Spotify successfully created playlist")
             res = response.json()
             self.playlist_id = res['id']
+            print(res['id'])
+        else:
+            print(response.content)
+    
+    def add_songs_to_playlist(self):
+        data = {
+            "uris": self.uris,
+            "position": 0
+        }
+        data = json.dumps(data)
+        url = f"https://api.spotify.com/v1/playlists/{self.playlist_id}/tracks"
+        response = requests.post(url, headers = self.headers, data = data)
+        if response.status_code == 201:
+            print("Successfully added songs to playlist")
         else:
             print(response.content)
 
-    def add_songs_to_playlist(self):
-        uri_list = json.dumps(self.uris)
-        url = f"https://api.spotify.com/v1/playlists/{self.playlist_id}/tracks"
-        response = requests.post(url, data = uri_list, headers=self.headers)
-        print(response.status_code, response.content)
-        if response.status_code == 201:
-            print("Song added successfully")
 
-    def list_songs_in_playlist(self):
-        pass
+        
 
 d = lastFmSpotify()
 d.fetch_songs_from_lastfm()
+d.get_uri_from_spotify()
 d.create_spotify_playlist()
 d.add_songs_to_playlist()
--- spotify/secrets.py
+++ spotify/secrets.py
@@ -2,7 +2,7 @@ def spotify_user_id():
     return '31bymk7hg5t4ej2hxc4m76abxcoy'
 
 def spotify_token():
-    return 'BQC1ABzYUMh2mb2__6_kv5IpEEzQ99YVHw75jlsRfFyZttLgzDkCF3z7k1dMKu3SAdUVaToL97kl1VHMsdnE7PdHW1Ov-UPdbC0SNrQUlUZvwERSqUKGcavZrkCwjvEvbWV5E_rROx2PhPW-ft2SBDoRkzV4ZJ7dBsMTHdgPaMCxvEk6VbQCqZQRa8sskuOepwaM_lrShPbr-X8Gmvb8MztVpyIr-bnZ_WTAX_a0M6IrCXe0oq0'
+    return 'BQAXKcQaAu1DjvAghNl5RxKt-hYz70XevsgemilpOlEu8CCl0r9EFd_64-hWGk6344BBJdPOX3fSEJfZL3ovDF76nT1-Gt4iEYWW3xKg4E_A2JE9DP3rsCEgVNjtrB6hRhVIp4lbN03S1H54AnrFlGOYycwk1ejd8nu9FRmQszOIaGmxC1dm1ysKD1o9r5DFowkNYashRglxU7RWuB0b2tf-zzPrnmgp9TYhN8F-M45lRDXgJxg'
 
 def last_fm_api_key():
     return 'f16cd08c7639f56ecfdfafd183c1b621'
